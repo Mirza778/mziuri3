fd5f0b010a293581474904c6267087b156037bd5
Mirza778 <mirza5454mo@gmail.com>
2025-10-21 23:16:39 +0400

    Update unique_letters.py

--- unique_letters.py
+++ unique_letters.py
@@ -1,3 +1,8 @@
-def all_unique_letters(s):
-    return len(set(s)) == len(s)
-
+def unique(s):
+    seen = set()
+    for i in s:
+        if i in seen:
+            return False
+        seen.add(i)
+    return True
+print(uniqe(hello))
